8d476b8d6a0e0c399e0ba370b5ea1e050aed5e9e
Peter Navarrete <navarrep1@newpaltz.edu>
2023-05-12 08:32:58 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use macroquad::prelude::*;
+
+
+const PLAYER_SIZE: Vec2 = Vec2::from_array([150f32, 40f32]);
+
+struct Player {
+    rect: Rect,
+}
+
+impl Player {
+    pub fn new() -> Self {
+        Self {
+            rect: Rect::new(
+                      screen_width() * 0.5f32 - PLAYER_SIZE.x*0.5f32,
+                      screen_height() - 100f32,
+                      PLAYER_SIZE.x,
+                      PLAYER_SIZE.y
+                      )
+        }
+    }
+}
+
+
+#[macroquad::main("breakout")]
+async fn main() {
+    
+    let player_rect = Rect::new(
+        screen_width() * 0.5f32 - PLAYER_SIZE.x * 0.5f32,
+        screen_height() - 100f32,
+        PLAYER_SIZE.x,
+        PLAYER_SIZE.y,
+        );
+
+
+    loop {
+
+        clear_background(WHITE);
+        draw_rectangle(player_rect.x, player_rect.y,
+                       player_rect.w, player_rect.h, DARKGRAY);
+
+        next_frame().await;
+
+    }
+}
