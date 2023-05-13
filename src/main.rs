60c96f6ea3440179b05c10b3c46d67691178b930
Peter Navarrete <peterabcs@gmail.com>
2023-05-13 08:17:14 -0400

    block struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use macroquad::prelude::*;
 
 
-const PLAYER_SIZE: Vec2 = Vec2::from_array([150f32, 40f32]);
+const PLAYER_SIZE: Vec2 = Vec2::from_array([150f32, 30f32]);
+const PLAYER_SPEED: f32 = 700f32;
+const BLOCK_SIZE: Vec2 = Vec2::from_array([100f32, 25f32]);
 
 struct Player {
     rect: Rect,
@@ -21,18 +23,67 @@ impl Player {
     pub fn draw(&self) {
         draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, BLUE);
     }
+
+    pub fn update(&mut self, dt: f32) {
+        let mut x_move = 0f32;
+        if is_key_down(KeyCode::Left) {
+            x_move -= 1f32;
+        }
+        if is_key_down(KeyCode::Right) {
+            x_move += 1f32;
+        }
+        self.rect.x += x_move * dt * PLAYER_SPEED;
+
+        if self.rect.x < 0f32 {
+            self.rect.x = 0f32;
+        } 
+        
+        if self.rect.x > screen_width() - self.rect.w {
+            self.rect.x = screen_width() - self.rect.w;
+        }
+
+    }
 }
 
+struct Block {
+    rect: Rect,
+}
+
+
+impl Block {
+    pub fn new(pos: Vec2) -> Self {
+        Self {
+            rect: Rect::new(pos.x, pos.y, BLOCK_SIZE.x, BLOCK_SIZE.y)
+        }
+    }
+    pub fn draw(&self) {
+        draw_rectangle(self.rect.x, self.rect.y, self.rect.w, self.rect.h, BLUE)
+    }
+}
 
 #[macroquad::main("breakout")]
 async fn main() {
     
-    let player = Player::new(); 
+    let mut player = Player::new(); 
+    let mut blocks = Vec::new();
+    let (width, height) = (6,6);
+    let padding = 5f32;
+    let total_block_size = BLOCK_SIZE + vec2(padding, padding);
+    let board_start_pos = vec2((screen_width() - (total_block_size.x * width as f32))*0.5f32, 50f32);
+    
 
+    for i in 0..width * height {
+        let block_x = (i % width) as f32 * total_block_size.x;
+        let block_y = (i / width) as f32 * total_block_size.y;
+        blocks.push(Block::new(board_start_pos + vec2(block_x, block_y)));
+    }
     loop {
-
+        player.update(get_frame_time());
         clear_background(WHITE);
         player.draw();
+        for block in blocks.iter() {
+            block.draw();
+        }
         next_frame().await;
 
     }
